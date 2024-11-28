f23a6b5c6734b102acee868d2f80c50cd45d7465
Plane <bf.plane@proton.me>
2024-11-28 19:20:33 +0100

    feat: add version validator

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,14 @@
+import { VersionValidation } from '@/src/components/VersionValidation.tsx';
 import instance from '@/src/i18n.ts';
 import { Toaster } from '@betfinio/components/ui';
 import { createRootRoute } from '@tanstack/react-router';
 import { Root } from 'betfinio_app/root';
+import React from 'react';
 
 export const Route = createRootRoute({
 	component: () => (
 		<Root id={'luro'} instance={instance}>
+			<VersionValidation repository={'affiliate'} branch={import.meta.env.PUBLIC_BRANCH} current={import.meta.env.PUBLIC_DEPLOYED} />
 			<Toaster />
 		</Root>
 	),
