078772e329730eec0f7829278f06a97c5c6c0705
Plane <bf.plane@proton.me>
2024-12-13 14:46:13 +0100

    fix: tabs styles change

--- src/components/luro/BetsInfo.tsx
+++ src/components/luro/BetsInfo.tsx
@@ -8,28 +8,26 @@ export const BetsInfo = () => {
 	const { t } = useTranslation('luro', { keyPrefix: 'tabs' });
 
 	return (
-		<div className={'flex flex-col h-full'}>
-			<Tabs defaultValue={'bets'} className={'md:max-w-[350px]'}>
-				<TabsList className={'w-full bg-transparent justify-between gap-2 grid grid-cols-3'}>
-					<TabsTrigger value={'players'}>{t('players')}</TabsTrigger>
-					<TabsTrigger value={'bets'}>{t('bets')}</TabsTrigger>
-					<TabsTrigger value={'bonuses'}>{t('bonus')}</TabsTrigger>
-				</TabsList>
+		<Tabs defaultValue={'bets'} className={'flex flex-col h-full'}>
+			<TabsList className={'w-full bg-transparent justify-between gap-2 grid grid-cols-3'}>
+				<TabsTrigger value={'players'}>{t('players')}</TabsTrigger>
+				<TabsTrigger value={'bets'}>{t('bets')}</TabsTrigger>
+				<TabsTrigger value={'bonuses'}>{t('bonus')}</TabsTrigger>
+			</TabsList>
 
-				<div className={'py-3 mt-3 min-h-[250px] overflow-y-auto rounded-xl bg-background-light grow flex flex-col gap-5 border-border border'}>
-					<div className={'grow flex flex-col h-full'}>
-						<TabsContent value={'players'} className={'overflow-hidden'}>
-							<PlayersTab />
-						</TabsContent>
-						<TabsContent value={'bets'} className={'overflow-hidden'}>
-							<BetsTab />
-						</TabsContent>
-						<TabsContent value={'bonuses'} className={'overflow-hidden'}>
-							<BonusTab />
-						</TabsContent>
-					</div>
+			<div className={'py-3 mt-3 min-h-[250px] overflow-y-auto rounded-xl bg-background-light grow flex flex-col gap-5 border-border border'}>
+				<div className={'grow flex flex-col h-full'}>
+					<TabsContent value={'players'} className={'overflow-hidden'}>
+						<PlayersTab />
+					</TabsContent>
+					<TabsContent value={'bets'} className={'overflow-hidden'}>
+						<BetsTab />
+					</TabsContent>
+					<TabsContent value={'bonuses'} className={'overflow-hidden'}>
+						<BonusTab />
+					</TabsContent>
 				</div>
-			</Tabs>
-		</div>
+			</div>
+		</Tabs>
 	);
 };
