2a68a973a966f2532049aa9e9919d728051a4dca
Plane <bf.plane@proton.me>
2025-01-16 17:14:23 +0100

    fix: rename global vars

--- src/global.ts
+++ src/global.ts
@@ -6,7 +6,7 @@ export const BETS_MEMORY = import.meta.env.PUBLIC_BETS_MEMORY_ADDRESS;
 export const PUBLIC_BRANCH = import.meta.env.PUBLIC_BRANCH;
 export const PUBLIC_DEPLOYED = import.meta.env.PUBLIC_DEPLOYED;
 
-export const staking_fee = 36n;
-export const bonus_fee = 50n;
+export const STAKING_FEE = 36n;
+export const BONUS_FEE = 50n;
 
-export const netCoef = 1000n - staking_fee - bonus_fee;
+export const NET_COEF = 1000n - STAKING_FEE - BONUS_FEE;
