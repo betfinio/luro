51afbbd5238f54ed54b51d74a7e4df26e250f0fe
bf-monster <bf.monster@proton.me>
2025-11-10 18:02:20 +0100

    feat: remove aws, migrate to ipfs

--- src/global.ts
+++ src/global.ts
@@ -1,12 +1,11 @@
+import type { Address } from 'viem';
 import type { LuroInterval } from './lib/types.ts';
 
-export const PARTNER = import.meta.env.PUBLIC_PARTNER_ADDRESS;
-export const ETHSCAN = import.meta.env.PUBLIC_ETHSCAN;
-export const LURO = import.meta.env.PUBLIC_LUCKY_ROUND_ADDRESS;
-export const LURO_5MIN = import.meta.env.PUBLIC_LUCKY_ROUND_5MIN_ADDRESS;
-export const BETS_MEMORY = import.meta.env.PUBLIC_BETS_MEMORY_ADDRESS;
-export const PUBLIC_BRANCH = import.meta.env.PUBLIC_BRANCH;
-export const PUBLIC_DEPLOYED = import.meta.env.PUBLIC_DEPLOYED;
+export const PARTNER: Address = import.meta.env.PUBLIC_PARTNER_ADDRESS as Address;
+export const ETHSCAN: string = import.meta.env.PUBLIC_ETHSCAN as string;
+export const LURO: Address = import.meta.env.PUBLIC_LUCKY_ROUND_ADDRESS as Address;
+export const LURO_5MIN: Address = import.meta.env.PUBLIC_LUCKY_ROUND_5MIN_ADDRESS as Address;
+export const BETS_MEMORY: Address = import.meta.env.PUBLIC_BETS_MEMORY_ADDRESS as Address;
 
 export const STAKING_FEE = 36n;
 export const BONUS_FEE = 50n;
